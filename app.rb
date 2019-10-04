4d3be4f3a3305a460308d255e3f0b155345f56e5
Guillermo Martinez <dumblemoe@gmail.com>
2019-10-04 18:00:32 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,8 +13,8 @@ class App < Sinatra::Base
     @pg = params[:pg]
     @sg = params[:sg]
     @pf = params[:pf]
-    @sf = params [:sf]
-    @c = [:c]
+    @sf = params[:sf]
+    @c = params[:c]
   erb :team 
   end
 
