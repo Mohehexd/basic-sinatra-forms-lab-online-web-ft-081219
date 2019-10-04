e63241b0f6e2ccd84c5f76d88419e8fe57f1840a
Guillermo Martinez <dumblemoe@gmail.com>
2019-10-04 17:39:11 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,5 +2,13 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do 
+    
+    erb :newteam
+
+  post '/team' do
+    @team = ['Coach: Louis Armstrong', 'Point Guard: Ella Fitzgerald', 'Shooting Guard: Miles Davis', 'Small Forward: Dave Brubeck', 'Power Forward: John Coltrane', 'Center: Sonny Rollins']
+    erb :team 
+  end
 
 end
